27017808b2347330f812f31f81c7b75956e7a703
IdenTiclla <iden.ticlla@gmail.com>
2021-11-29 16:03:35 -0400

    agregando estilos bootstrap

--- django_password_generator/urls.py
+++ django_password_generator/urls.py
@@ -20,8 +20,8 @@ from generator import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
-        "about/", views.about
+        "about/", views.about, name="about"
     ),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('generate-password',views.password, name='password'),
 ]
